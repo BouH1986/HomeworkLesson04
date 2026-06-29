9abe220aaea98f4fdd5883ea3ef8471810573d14
BouH1986 <jack-2006@mail.ru>
2026-06-29 12:41:09 +0400

    copy from task

--- src/Main.java
+++ src/Main.java
@@ -38,4 +38,13 @@ public class Main {
         System.out.println("Программа завершена!");
 
     }
+    public static int taxEarningsMinusSpendings(int earnings, int spendings) {
+        int tax = (earnings - spendings) * 15 / 100;
+        if (tax >= 0) {
+            return tax;
+        } else {
+            // если расходы оказались больше, то налог посчитается отрицательным
+            return 0;
+        }
+    }
 }
