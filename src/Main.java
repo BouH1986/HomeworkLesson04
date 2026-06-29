174f1c242dda27e6858dd09d2f9260a22f2c1c6b
BouH1986 <jack-2006@mail.ru>
2026-06-29 15:04:00 +0400

    оптимизация

--- src/Main.java
+++ src/Main.java
@@ -62,16 +62,6 @@ public class Main {
         System.out.println("Программа завершена!");
     }
 
-    public static int taxEarnings(int earnings, int spendings) {
-        int tax = earnings * 6 / 100;
-        if (tax >= 0) {
-            return tax;
-        } else {
-            // если расходы оказались больше, то налог посчитается отрицательным
-            return 0;
-        }
-    }
-
     public static int taxEarningsMinusSpendings(int earnings, int spendings) {
         int tax = (earnings - spendings) * 15 / 100;
         if (tax >= 0) {
